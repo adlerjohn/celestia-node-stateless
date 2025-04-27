acf84ca534f0f01b46305481cc55803e93932114
John Adler <john.adler@protonmail.ch>
2025-04-27 11:15:33 -0400

    Add block fetcher.

--- cmd/celestia/main.go
+++ cmd/celestia/main.go
@@ -1,15 +1,145 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"os"
+	"strconv"
 
+	"github.com/gogo/protobuf/proto"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	coregrpc "github.com/tendermint/tendermint/rpc/grpc"
+	"github.com/tendermint/tendermint/types"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-var (
-	client coregrpc.BlockAPIClient
-)
+type SignedBlock struct {
+	Header       *types.Header       `json:"header"`
+	Commit       *types.Commit       `json:"commit"`
+	Data         *types.Data         `json:"data"`
+	ValidatorSet *types.ValidatorSet `json:"validator_set"`
+}
 
 func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		os.Exit(0)
+	}
+
+	// First argument is the core address
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	conn, err := grpc.NewClient(args[0], opts...)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ctx := context.WithoutCancel(context.Background())
+
+	client := coregrpc.NewBlockAPIClient(conn)
+
+	switch args[1] {
+	case "share":
+		fmt.Println("share")
+	case "blob":
+		fmt.Println("blob")
+	case "block":
+		fmt.Println("block")
+		height, err := strconv.Atoi(args[2])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		stream, err := client.BlockByHeight(ctx, &coregrpc.BlockByHeightRequest{Height: int64(height)})
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		block, err := receiveBlockByHeight(stream)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(block)
+	default:
+		os.Exit(0)
+	}
 	os.Exit(0)
+}
+
+func receiveBlockByHeight(streamer coregrpc.BlockAPI_BlockByHeightClient) (
+	*SignedBlock,
+	error,
+) {
+	parts := make([]*tmproto.Part, 0)
+
+	// receive the first part to get the block meta, commit, and validator set
+	firstPart, err := streamer.Recv()
+	if err != nil {
+		return nil, err
+	}
+	commit, err := types.CommitFromProto(firstPart.Commit)
+	if err != nil {
+		return nil, err
+	}
+	validatorSet, err := types.ValidatorSetFromProto(firstPart.ValidatorSet)
+	if err != nil {
+		return nil, err
+	}
+	parts = append(parts, firstPart.BlockPart)
+
+	// receive the rest of the block
+	isLast := firstPart.IsLast
+	for !isLast {
+		resp, err := streamer.Recv()
+		if err != nil {
+			return nil, err
+		}
+		parts = append(parts, resp.BlockPart)
+		isLast = resp.IsLast
+	}
+	block, err := partsToBlock(parts)
+	if err != nil {
+		return nil, err
+	}
+	return &SignedBlock{
+		Header:       &block.Header,
+		Commit:       commit,
+		Data:         &block.Data,
+		ValidatorSet: validatorSet,
+	}, nil
+}
+
+// partsToBlock takes a slice of parts and generates the corresponding block.
+// It empties the slice to optimize the memory usage.
+func partsToBlock(parts []*tmproto.Part) (*types.Block, error) {
+	partSet := types.NewPartSetFromHeader(types.PartSetHeader{
+		Total: uint32(len(parts)),
+	})
+	for _, part := range parts {
+		ok, err := partSet.AddPartWithoutProof(&types.Part{Index: part.Index, Bytes: part.Bytes})
+		if err != nil {
+			return nil, err
+		}
+		if !ok {
+			return nil, err
+		}
+	}
+	pbb := new(tmproto.Block)
+	bz, err := io.ReadAll(partSet.GetReader())
+	if err != nil {
+		return nil, err
+	}
+	err = proto.Unmarshal(bz, pbb)
+	if err != nil {
+		return nil, err
+	}
+	block, err := types.BlockFromProto(pbb)
+	if err != nil {
+		return nil, err
+	}
+	return block, nil
 }
